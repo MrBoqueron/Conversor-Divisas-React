f7776c8c98b4803064be37aa886c4945fb57ef07
Fabio Tudero <fabiotudero@gmail.com>
2022-11-10 12:56:31 +0000

    1.0.1   --Update 3 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
-import euro from "./components/euroToDolar";
-import "./styles.css";
+import { useState } from 'react';
+import './styles.css';
 
 export default function App() {
+  const [euros, setEuros] = useState('');
+
+  const handleInput = ({ target }) => {
+    setEuros(target.value);
+  };
+
   return (
-    <div className="App">
+    <div className='App'>
       <h1>CONVERSOR DE DIVISAS</h1>
 
-      <div className="container1">
-        <label>Convertir euros a dolares</label>
+      <div className='container1'>
+        <div className='tile'>
+          <label>Convertir euros a dolares</label>
+        </div>
         <br />
         <label>Euros:</label>
-        <input type="text" name="" id="" className="input" />
+        <input
+          type='text'
+          name=''
+          id=''
+          className='input'
+          onChange={handleInput}
+        />
         <br />
         <label>Dolares:</label>
-        <input type="text" name="" id="" className="input" disabled />
-        <button className="boton-convertir">Convertir</button>
+        <input type='text' name='' id='' className='input' disabled />
+        <button className='boton-convertir'>Convertir</button>
       </div>
     </div>
   );
